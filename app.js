2182923f2cf5b8335b829ca5c8613ba25652c051
filippoimarisio <filippo.imarisio@gmail.com>
2019-03-19 20:45:14 +0100

    users schema and relations

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Match = require('./models/match');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,11 +23,22 @@ app.use('/graphql', graphqlHttp({
       date: String!
     }
 
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
+
     input MatchInput {
       title: String!
       description: String!
       price: Float!
       date: String!
+    }
+
+    input UserInput {
+      email: String!
+      password: String!
     }
 
     type RootQuery {
@@ -34,6 +47,7 @@ app.use('/graphql', graphqlHttp({
 
     type RootMutation {
       createMatch(matchInput: MatchInput): Match
+      createUser(userInput: UserInput): User
     }
 
     schema {
@@ -58,20 +72,55 @@ app.use('/graphql', graphqlHttp({
         title: args.matchInput.title,
         description: args.matchInput.description,
         price: +args.matchInput.price,
-        date: new Date (args.matchInput.date)
-      })
+        date: new Date (args.matchInput.date),
+        creator: '5c91405d73a94b46cb9141c9'
+      });
+      let createdMatch;
       return match
         .save()
         .then(result => {
-          console.log(result);
-          return {...result._doc};
+          createdMatch = {...result._doc, _id: result.id };
+          return User.findById('5c91405d73a94b46cb9141c9');
+        })
+        .then(user => {
+          if(!user) {
+            throw new Error('User not found.')
+          }
+          user.createdMatches.push(match);
+          return user.save();
+        })
+        .then(result => {
+          return createdMatch;
         })
         .catch(err => {
           console.log(err);
           throw err;
       });
       return match;
-    }
+    },
+    createUser: args => {
+      return User.findOne({email: args.userInput.email})
+        .then(user => {
+          if(user) {
+            throw new Error('User already exits.')
+          }
+          return bcrypt.hash(args.userInput.password, 12)
+        })
+        .then(hashedPassword => {
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword
+          });
+          
+          return user.save();
+        })
+        .then(result => {
+          return { ...result._doc, password:null, _id: result.id };
+        })
+        .catch(err => {
+          throw err;
+        });
+   }
   },
   graphiql: true
 }));
